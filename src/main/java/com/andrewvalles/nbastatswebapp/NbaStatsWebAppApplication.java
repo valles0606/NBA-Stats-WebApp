115dce8f9a37206397dbaff837906bc731b83cc0
Andrew <andrewvallescsw@gmail.com>
2026-05-30 23:50:25 -0700

    Create New NBA WebApp

---
+++ src/main/java/com/andrewvalles/nbastatswebapp/NbaStatsWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.andrewvalles.nbastatswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaStatsWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbaStatsWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrewvalles/nbastatswebapp/NbaStatsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewvalles.nbastatswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaStatsWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
