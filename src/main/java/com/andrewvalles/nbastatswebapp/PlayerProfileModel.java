f6e52042dba36c7f92dfe76d532b7d553c458e59
Andrew <andrewvallescsw@gmail.com>
2026-06-01 13:26:25 -0700

    Create New NBA WebApp Create model for player profiles and stats. Add endpoints to retrieve this data.

--- src/main/java/com/andrewvalles/nbastatswebapp/PlayerProfileModel.java
+++ src/main/java/com/andrewvalles/nbastatswebapp/PlayerProfileModel.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class PlayerProfileModel extends PlayerPreviewModel{
     @JsonProperty("profile")
     private ProfileModel profile;
-
+//temp
     public ProfileModel getProfile() {
         return profile;
     }
